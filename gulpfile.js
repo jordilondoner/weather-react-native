9ff7cf76beafea38e33979161114b268394fa120
Jordi Mundó <jordi.mundo@sainsburys.co.uk>
2016-02-20 08:18:54 +0000

    setting up prettify for auto formattig

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,8 @@
 const gulp = require('gulp');
+const prettify = require('gulp-js-prettify');
+
+gulp.task('prettify', function() {
+    gulp.src('./src/*.js')
+        .pipe(prettify({collapseWhitespace: true}))
+        .pipe(gulp.dest('./src'))
+});
